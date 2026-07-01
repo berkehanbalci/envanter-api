f294ad0e8d22c4733f7e4db31661b20c5993f578
Berkehan Balcı <github-berkehanbalci@gmail.com>
2026-07-01 21:14:32 +0300

    CRUD işlemleri ekle: POST, PUT, DELETE

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import sqlite3
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Urun(BaseModel):
+    ad: str 
+    fiyat: float
+    stok: int
+
 @app.get("/")
 def ana_sayfa():
     return {"mesaj": "Envanter API çalışıyor!"}
@@ -23,5 +30,38 @@ def urunleri_getir():
             "stok": satir[3]
         }
         urunler.append(urun)
-    return urunler    
+    return urunler   
+
+@app.post("/urunler")
+def urun_ekle(urun: Urun):
+    baglanti = sqlite3.connect("envanter.db")
+    imlec = baglanti.cursor()
+    imlec.execute(
+        "INSERT INTO urunler (ad, fiyat, stok)  VALUES (?, ?, ?)",
+        (urun.ad, urun.fiyat, urun.stok)
+    ) 
+    baglanti.commit()
+    baglanti.close()
+    return{"mesaj": f"{urun.ad} eklendi!"}
+
+@app.delete("/urunler/{urun_id}")
+def urun_sil(urun_id: int):
+    baglanti = sqlite3.connect("envanter.db")
+    imlec = baglanti.cursor()
+    imlec.execute("DELETE FROM urunler WHERE id = ?", (urun_id,))
+    baglanti.commit()
+    baglanti.close()
+    return {"mesaj": f"{urun_id} numaralı ürün silindi!"}
+
+@app.put("/urunler/{urun_id}")
+def urun_guncelle(urun_id: int, urun: Urun):
+    baglanti = sqlite3.connect("envanter.db")
+    imlec = baglanti.cursor()
+    imlec.execute(
+        "UPDATE urunler SET ad = ?, fiyat = ?, stok = ? WHERE id = ?",
+        (urun.ad, urun.fiyat, urun.stok, urun_id)
+    )
+    baglanti.commit()
+    baglanti.close()
+    return {"mesaj": f"{urun_id} numaralı ürün güncellendi!"}
     
